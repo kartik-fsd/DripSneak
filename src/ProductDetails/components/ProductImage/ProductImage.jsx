731827354f487e72c6dd0f7aac49da554b03189b
kartik-fsd <kartik.ks@int.taskmo.com>
2023-07-25 12:44:18 +0530

    fixed the bugs

--- src/ProductDetails/components/ProductImage/ProductImage.jsx
+++ src/ProductDetails/components/ProductImage/ProductImage.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { useMediaQuery } from "react-responsive";
+
 import "./ProductImage.css";
 import Tilt from "react-parallax-tilt";
-import ReactImageMagnify from "react-image-magnify";
+
+//import ReactImageMagnify from "react-image-magnify";
 
 export const ProductImage = ({ selectedProduct }) => {
-  const isDesktopOrLaptop = useMediaQuery({
-    query: "(min-width: 992px)",
-  });
-
   return (
     <div
       style={{
@@ -18,41 +15,14 @@ export const ProductImage = ({ selectedProduct }) => {
         aspectRatio: "16/9",
       }}
     >
-      {isDesktopOrLaptop ? (
-        <div>
-          <ReactImageMagnify
-            smallImage={{
-              alt: "Small Image",
-              src: selectedProduct?.img,
-              width: 610,
-              height: 600,
-            }}
-            largeImage={{
-              src: selectedProduct?.img,
-              width: 1200,
-              height: 900,
-            }}
-            lensStyle={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
-            enlargedImagePosition="over"
-            isActivatedOnTouch={true}
-            style={{
-              zIndex: 99,
-              maxWidth: "100%",
-              maxHeight: "100%",
-              objectFit: "contain",
-            }}
-          />
-        </div>
-      ) : (
-        <Tilt
-          tiltEnable={true}
-          scale={1.05}
-          transitionSpeed={1000}
-          className="product-details-image"
-        >
-          <img src={selectedProduct?.img} alt="drip" />
-        </Tilt>
-      )}
+      <Tilt
+        tiltEnable={true}
+        scale={1.05}
+        transitionSpeed={1000}
+        className="product-details-image"
+      >
+        <img src={selectedProduct?.img} alt="drip" />
+      </Tilt>
     </div>
   );
 };
